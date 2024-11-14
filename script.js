a7946e9ad8dc469b2907674cc812a56ee4858ada
Rachit-Dhaka <rachitdhaka29@gmail.com>
2024-11-14 20:28:17 +0530

    Count Down Timer Application

---
+++ script.js
@@ -0,0 +1,31 @@
+
+
+const startTime=new Date().getTime();
+const endTime=new Date("1 Dec , 2024 10:00:00").getTime();
+
+
+let a=setInterval(function updateTimer(){
+    const CurrentTime=new Date().getTime();
+
+    const distancePending=endTime-CurrentTime;
+
+    const OneDayInMilli=(24*60*60*1000);
+    const OneHourInMilli=(60*60*1000);
+    const OneMinInMilli=(60*1000);
+    const OneSecondInMilli=(1000);
+
+    const days=Math.floor((distancePending/OneDayInMilli));
+    const hrs=Math.floor((distancePending%OneDayInMilli/OneHourInMilli));
+    const mins=Math.floor((distancePending%OneHourInMilli/OneMinInMilli));
+    const secs=Math.floor((distancePending%OneMinInMilli/OneSecondInMilli));
+
+    document.getElementById("Day").innerHTML=days;
+    document.getElementById("Hours").innerHTML=hrs;
+    document.getElementById("Minutes").innerHTML=mins;
+    document.getElementById("seconds").innerHTML=secs;
+
+
+
+},1000);
+
+
